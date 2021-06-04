5083f828b9a20efbcdbe67b446fb22aee55ab82e
Marley Alford <marleycalford@gmail.com>
2021-06-04 02:02:36 -0400

    Exercise 5.1

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState([""]);
+  const [password, setPassword] = useState([""]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginView = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <h2>log in to application</h2>
+        <label htmlFor="username">Username</label>
+        <input
+          name="username"
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
+
+  const blogView = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  return (
+    <div>
+      {user === null && loginView()}
+      {user !== null && blogView()}
+    </div>
+  );
+};
+
+export default App;
