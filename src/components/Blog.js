926bdb4bdfc307fcf3bfa656f94274a9ae90bfe8
Marley Alford <marleycalford@gmail.com>
2021-06-27 22:48:14 -0400

    Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  let buttonTitle = showDetails ? "hide" : "view";
+
+  const details = (
+    <div>
+      {blog.url} <br />
+      likes {blog.likes} <br />
+      {blog.user.name} <br />
+    </div>
+  );
+
+  return (
+    <div className="blog-div">
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {buttonTitle}
+      </button>
+      {showDetails && details}
+    </div>
+  );
+};
 
 export default Blog;
